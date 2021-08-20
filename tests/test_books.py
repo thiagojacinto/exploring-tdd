ae3175c7f438b078116fe42db20cc754b033670f
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-08-19 22:56:42 -0300

    tests: renaming to use Spies and Stubs

--- tests/test_books.py
+++ tests/test_books.py
@@ -13,27 +13,27 @@ def test_when_search_by_author_should_return_a_string():
 
 def test_when_search_by_author_should_prepare_a_request_with_that_data():
     author_name = "Itamar Vieira Junior"
-    with patch("collection.books.prepare_request") as duble_prepare_request:
+    with patch("collection.books.prepare_request") as spy_prepare_request:
         search_books(author_name)
-        duble_prepare_request.assert_called_once_with(author_name)
+        spy_prepare_request.assert_called_once_with(author_name)
 
 def test_when_search_with_author_name_should_use_prepare_request_return_as_input_to_get_url():
     author_name = "Itamar Vieira Junior"
     dados = { 'author_name': author_name }
-    with patch("collection.books.prepare_request") as duble_prepare_request:
-        duble_prepare_request.return_value = dados
-        with patch("collection.books.get_url") as duble_get_url:
+    with patch("collection.books.prepare_request") as stub_prepare_request:
+        stub_prepare_request.return_value = dados
+        with patch("collection.books.get_url") as spy_get_url:
             search_books(author_name)
-            duble_get_url.assert_called_once_with("https://go.to", dados)
+            spy_get_url.assert_called_once_with("https://go.to", dados)
 
 def test_when_search_with_author_name_should_use_get_url_return_as_input_to_execute():
     author_name = "Itamar Vieira Junior"
     url = "https://go.to"
-    with patch("collection.books.get_url") as duble_get_url:
-        duble_get_url.return_value = url
-        with patch("collection.books.execute") as duble_execute:
+    with patch("collection.books.get_url") as stub_get_url:
+        stub_get_url.return_value = url
+        with patch("collection.books.execute") as spy_execute:
             search_books(author_name)
-            duble_execute.assert_called_once_with(url)
+            spy_execute.assert_called_once_with(url)
 
 class StubHttpResponse:
 
